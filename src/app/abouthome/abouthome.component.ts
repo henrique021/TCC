11022073cced495063ac9cf3490ade2db133f8b1
Henrique <hrgonnzaga@gmail.com>
2020-07-11 20:10:02 -0300

    erro carrosel

---
+++ src/app/abouthome/abouthome.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-abouthome',
+  templateUrl: './abouthome.component.html',
+  styleUrls: ['./abouthome.component.css']
+})
+export class AbouthomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { VitrineComponent } from './vitrine/vitrine.component';
+import { FooterComponent } from './footer/footer.component';
+import { AbouthomeComponent } from './abouthome/abouthome.component';
+import { DetailComponent } from './detail/detail.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    CarouselComponent,
+    VitrineComponent,
+    FooterComponent,
+    AbouthomeComponent,
+    DetailComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
